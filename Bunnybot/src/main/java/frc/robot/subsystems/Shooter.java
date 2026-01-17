c9f3fdcfa76839bf5454551cc6e51e40d02dd384
ashwinpotluri <ashwinpotluri01@gmail.com>
2026-01-17 16:32:08 -0500

    Changed the shooter Motor to Neo's

--- Bunnybot/src/main/java/frc/robot/subsystems/Shooter.java
+++ Bunnybot/src/main/java/frc/robot/subsystems/Shooter.java
@@ -11,21 +11,22 @@ import frc.robot.Constants.*;
 import frc.robot.helpers.PIDProfile;
 import frc.robot.helpers.motor.NewtonMotor;
 import frc.robot.helpers.motor.talonfx.KrakenX60Motor;
+import frc.robot.helpers.motor.spark.SparkFlexMotor;
 
 
 public class Shooter extends SubsystemBase{
-    private KrakenX60Motor LeftShooterMotor;
-    private KrakenX60Motor RightShooterMotor;
+    private SparkFlexMotor LeftShooterMotor;
+    private SparkFlexMotor RightShooterMotor;
     private PIDProfile MotorPID;
 
     public Shooter(){
-        LeftShooterMotor = new KrakenX60Motor(SHOOTER.LEFT_SHOOTER_MOTOR, true);
-        RightShooterMotor = new KrakenX60Motor(SHOOTER.RIGHT_SHOOTER_MOTOR, false);
+        LeftShooterMotor = new SparkFlexMotor(SHOOTER.LEFT_SHOOTER_MOTOR, true);
+        RightShooterMotor = new SparkFlexMotor(SHOOTER.RIGHT_SHOOTER_MOTOR, false);
         MotorPID = new PIDProfile();
         MotorPID.setPID(SHOOTER.MOTOR_P, SHOOTER.MOTOR_I, SHOOTER.MOTOR_D);
         LeftShooterMotor.setFollowerTo(RightShooterMotor);
         LeftShooterMotor.withGains(MotorPID);
-        LeftShooterMotor.configureMotionMagic(SHOOTER.MAX_ACCELERATION, SHOOTER.CRUISE_VELOCITY);
+        //LeftShooterMotor.configureMotionMagic(SHOOTER.MAX_ACCELERATION, SHOOTER.CRUISE_VELOCITY);
     }
 
     public void runAtSpeed(double desiredRPM){
