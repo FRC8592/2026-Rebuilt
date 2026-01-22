606f147a9a35c19f354fb1fc544a7c0f2ed3e542
renegoswami <renegoswami@gmail.com>
2026-01-21 19:05:09 -0500

    Switched the code location

---
+++ robot-rebuilt/src/main/java/frc/robot/Intake.java
@@ -0,0 +1,68 @@
+
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.Logger;
+
+//import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.helpers.motor.NewtonMotor;
+import frc.robot.helpers.motor.NewtonMotor.IdleMode;
+import frc.robot.helpers.motor.spark.SparkFlexMotor;
+// import frc.robot.helpers.motor.spark.*;
+import frc.robot.helpers.motor.talonfx.Falcon500Motor;
+
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;   
+import frc.robot.Constants.*;
+// import frc.robot.helpers.motor.NewtonMotor;
+// import frc.robot.helpers.motor.NewtonMotor.IdleMode;
+// import frc.robot.helpers.motor.spark.ThisIsSparkFlexMotor;
+// import frc.robot.helpers.PIDProfile;
+import frc.robot.helpers.motor.NewtonMotor;
+
+public class Intake extends SubsystemBase{
+    // Drive intake wheels
+    private NewtonMotor intakeMotor;
+    // Lift to deploy and stow intake
+
+   public Intake() {
+   
+    // Create the intake wheel and lift motors
+    intakeMotor = new SparkFlexMotor(CAN.INTAKE_MOTOR_CAN_ID, false);
+
+
+   }
+
+   public void setPercentOut(NewtonMotor intakeMotor, double percent){
+        intakeMotor.setPercentOutput(percent);
+
+   }
+
+   public void stop(NewtonMotor intakeMotor){
+        intakeMotor.setPercentOutput(0);
+   }
+
+    public Command setIntakeCommand (double percent){
+    return this.run(() -> setPercentOut(intakeMotor, percent));
+    }
+
+    public Command setOuttakeCommand (double percent){
+    return this.run (() -> setPercentOut(intakeMotor, percent));
+    }
+
+   public Command stopIntakeCommand(){
+    return this.runOnce(() -> stop(intakeMotor));
+   }
+
+   public double rotationstoDegrees(double motorRotations){
+    return motorRotations * 1/(INTAKE.INTAKE_DEGREES_TO_MOTOR_ROTATIONS);
+   }
+
+   public double degreestoRotations(double degrees){
+    return degrees * INTAKE.INTAKE_DEGREES_TO_MOTOR_ROTATIONS;
+   }
+ 
+
+}
--- robot-rebuilt/src/main/java/frc/robot/subsystems/Intake.java
+++ robot-rebuilt/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,67 +1,92 @@
 package frc.robot.subsystems;
 
-import org.littletonrobotics.junction.Logger;
-
-//import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.helpers.motor.NewtonMotor;
-import frc.robot.helpers.motor.NewtonMotor.IdleMode;
-import frc.robot.helpers.motor.spark.SparkFlexMotor;
-// import frc.robot.helpers.motor.spark.*;
-import frc.robot.helpers.motor.talonfx.Falcon500Motor;
 
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;   
+import org.littletonrobotics.junction.Logger;
+
+import java.lang.Math;
+
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
 import frc.robot.Constants.*;
-// import frc.robot.helpers.motor.NewtonMotor;
-// import frc.robot.helpers.motor.NewtonMotor.IdleMode;
-// import frc.robot.helpers.motor.spark.ThisIsSparkFlexMotor;
-// import frc.robot.helpers.PIDProfile;
+import frc.robot.helpers.PIDProfile;
 import frc.robot.helpers.motor.NewtonMotor;
+import frc.robot.helpers.motor.talonfx.KrakenX60Motor;
+import frc.robot.helpers.motor.spark.SparkFlexMotor;
+
 
 public class Intake extends SubsystemBase{
-    // Drive intake wheels
-    private NewtonMotor intakeMotor;
-    // Lift to deploy and stow intake
+    private SparkFlexMotor IntakeMotor;
 
-   public Intake() {
-   
-    // Create the intake wheel and lift motors
-    intakeMotor = new SparkFlexMotor(CAN.INTAKE_MOTOR_CAN_ID, false);
+    private PIDProfile MotorPID;
+    private double POld = INTAKE.MOTOR_P;
+    private double IOld = INTAKE.MOTOR_I;
+    private double DOld = INTAKE.MOTOR_D;
 
-
-   }
-
-   public void setPercentOut(NewtonMotor intakeMotor, double percent){
-        intakeMotor.setPercentOutput(percent);
-
-   }
-
-   public void stop(NewtonMotor intakeMotor){
-        intakeMotor.setPercentOutput(0);
-   }
-
-    public Command setIntakeCommand (double percent){
-    return this.run(() -> setPercentOut(intakeMotor, percent));
+    public Intake(){
+        IntakeMotor = new SparkFlexMotor(INTAKE.INTAKE_MOTOR_CAN_ID, true);
+        MotorPID = new PIDProfile();
+        MotorPID.setSlot(0);
+        MotorPID.setPID(SHOOTER.MOTOR_P, SHOOTER.MOTOR_I, SHOOTER.MOTOR_D);
+        LeftShooterMotor.withGains(MotorPID);
+        LeftShooterMotor.setCurrentLimit(80);
+        //RightShooterMotor.setFollowerTo(LeftShooterMotor, true);
+        SmartDashboard.putNumber("P", 0.01);
+        SmartDashboard.putNumber("I", 0.0);
+        SmartDashboard.putNumber("D", 0.0);
+        SmartDashboard.putNumber("Vi", 3000);
+        //LeftShooterMotor.configureMotionMagic(SHOOTER.MAX_ACCELERATION, SHOOTER.CRUISE_VELOCITY);
     }
 
-    public Command setOuttakeCommand (double percent){
-    return this.run (() -> setPercentOut(intakeMotor, percent));
+    public void runAtSpeed(double desiredRPM){
+        double RPM = SmartDashboard.getNumber("Vi", 0);
+        System.out.println("Shooter method is running");
+        System.out.println("RPM Set " + RPM);
+        LeftShooterMotor.setVelocity(RPM);
     }
 
-   public Command stopIntakeCommand(){
-    return this.runOnce(() -> stop(intakeMotor));
-   }
+    public Command runAtSpeedCommand(){
+        double RPM = SmartDashboard.getNumber("Vi", 0);
+        return this.runOnce(() -> runAtSpeed(RPM));
+    }
+    public void updatePID(){
+        //System.out.println("Going into updatePID Method");
+        double P = SmartDashboard.getNumber("P", 0.001);
+        double I = SmartDashboard.getNumber("I", 0.0);
+        double D = SmartDashboard.getNumber("D", 0.0);
+        //System.out.println("P Value from SmartDashboard "+ P + " P Old Value " + POld);
+        if((P != POld) || (I!= IOld) || (D!= DOld)){
+            System.out.println("Going into if statement in updatePID method");
+            MotorPID.setPID(P, I, D);
+            LeftShooterMotor.withGains(MotorPID);
+            POld = P;
+            IOld = I;
+            DOld = D;
+        }
+    }
+    public void stopShooter(){
+        LeftShooterMotor.setPercentOutput(0);
+    }
 
-   public double rotationstoDegrees(double motorRotations){
-    return motorRotations * 1/(INTAKE.INTAKE_DEGREES_TO_MOTOR_ROTATIONS);
-   }
+    public Command stopShooterCommand(){
+        return this.runOnce(() -> stopShooter());
+    }
 
-   public double degreestoRotations(double degrees){
-    return degrees * INTAKE.INTAKE_DEGREES_TO_MOTOR_ROTATIONS;
-   }
- 
+    public double getVelocity(){
+        return LeftShooterMotor.getVelocityRPM();
+    }
+    //This is simply for calculation to get the ball landing in the center of the goal, based on the distance to the hub
+    //This is very much a theoretical implementation, simply putting in just the math
+    public double DistanceToRPM(double theta, double dis){
+        return (1/Math.cos(theta))* (Math.sqrt((0.5 * 9.81 * Math.pow(dis,2))/(SHOOTER.SHOOTER_HEIGHT + Math.tan(theta) - SHOOTER.HUB_HEIGHT)));
+    }
 
+    @Override
+    public void periodic(){
+        // updatePID();
+        Logger.recordOutput("Motor Velocity RPM", getVelocity());
+    }
+        
 }
