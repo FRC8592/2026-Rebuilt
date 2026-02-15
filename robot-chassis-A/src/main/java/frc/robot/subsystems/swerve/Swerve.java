6f2138ade834a47bff996a6b23dbd69fd09a09b2
yuii01 <whitecoralflame@gmail.com>
2026-02-15 16:04:47 -0500

    currently working on new swerve methods

--- robot-chassis-A/src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ robot-chassis-A/src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -2,9 +2,28 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.subsystems.swerve;
-import frc.robot.Constants.*;
+
+import frc.robot.Constants.SWERVE;
 import frc.robot.Robot;
 import frc.robot.helpers.SmoothingFilter;
+
+import java.util.List;
+
+import org.littletonrobotics.junction.Logger;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.FollowPathCommand;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
+import com.pathplanner.lib.util.DriveFeedforwards;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -12,20 +31,11 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
-import org.littletonrobotics.junction.Logger;
-
-import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
-import com.ctre.phoenix6.swerve.SwerveRequest;
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.config.PIDConstants;
-import com.pathplanner.lib.config.RobotConfig;
-import com.pathplanner.lib.controllers.PPHolonomicDriveController;
-
 public class Swerve extends SubsystemBase {
-
     private PIDController snapToController;
 
     private boolean isSlowMode;
@@ -33,33 +43,34 @@ public class Swerve extends SubsystemBase {
     private SmoothingFilter smoothingFilter;
     
     private CommandSwerveDrivetrain swerve;
+    
     private final SwerveRequest.FieldCentric fieldCentric = new SwerveRequest.FieldCentric()
-            .withDeadband(SWERVE.MAX_SPEED * 0.01).withRotationalDeadband(SWERVE.MAX_ANGULAR_RATE * 0.01) // Add a 1% deadband
-            .withDriveRequestType(DriveRequestType.Velocity); // Use closed-loop control for drive motors
+        .withDeadband(SWERVE.MAX_SPEED * 0.1).withRotationalDeadband(SWERVE.MAX_ANGULAR_RATE * 0.01) // Add a 1% deadband
+        .withDriveRequestType(DriveRequestType.Velocity); // Use closed-loop velocity control for drive motors
 
     private final SwerveRequest.RobotCentric robotCentric = new SwerveRequest.RobotCentric()
-        .withDeadband(SWERVE.MAX_SPEED * 0.01).withRotationalDeadband(SWERVE.MAX_ANGULAR_RATE * 0.01)
+        .withDeadband(SWERVE.MAX_SPEED * 0.1).withRotationalDeadband(SWERVE.MAX_ANGULAR_RATE * 0.01)
         .withDriveRequestType(DriveRequestType.Velocity);
-
-    public static ChassisSpeeds speedZero = new ChassisSpeeds();
-
-    private ChassisSpeeds currentSpeeds = new ChassisSpeeds();
 
     private RobotConfig config = null;
 
     public Swerve(CommandSwerveDrivetrain drivetrain) {
+        swerve = drivetrain;
+
         smoothingFilter = new SmoothingFilter(
             SWERVE.TRANSLATION_SMOOTHING_AMOUNT,
             SWERVE.TRANSLATION_SMOOTHING_AMOUNT,
             SWERVE.ROTATION_SMOOTHING_AMOUNT
         );
 
+        //TODO: check of the pid values are still valid
         snapToController = new PIDController(SWERVE.SNAP_TO_kP, SWERVE.SNAP_TO_kI, SWERVE.SNAP_TO_kD); // Turns the robot to a set heading
+        snapToController.enableContinuousInput(-Math.PI, Math.PI); //makes -pi and pi friendly neighbors :)
+        snapToController.setTolerance(Math.toRadians(2.0)); //prevent chatter and oscillation
 
-        swerve = drivetrain;
-
-        //PathPlanner AutoBuilder configuration below. 
+        //PathPlanner example AutoBuilder configuration code below. 
         //https://pathplanner.dev/pplib-build-an-auto.html
+
         try {
             config = RobotConfig.fromGUISettings(); 
         } catch (Exception e) {
@@ -69,17 +80,16 @@ public class Swerve extends SubsystemBase {
             return;
         }
 
-        // Configure AutoBuilder
         AutoBuilder.configure(
             this::getCurrentOdometryPosition, // Robot pose supplier
-            this::setKnownOdometryPose, // Method to reset odometry (will be called if your auto has a starting pose)
+            this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
             this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
             
             (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
             //TODO: replace pid constants
             new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
-                    new PIDConstants(SWERVE.DRIVE_KP, SWERVE.DRIVE_KI, SWERVE.DRIVE_KD), // Translation PID constants
-                    new PIDConstants(SWERVE.STEER_KP, SWERVE.STEER_KI, SWERVE.STEER_KD) // Rotation PID constants
+                    new PIDConstants(2.0, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(2.0, 0.0, 0.0) // Rotation PID constants
             ),
 
             config, // The robot configuration
@@ -99,13 +109,9 @@ public class Swerve extends SubsystemBase {
         );
     }
 
-    //TODO: calculate the robot relative speed instead
-    /**
-     * Gets robot relative speeds commanded to the robot (NOT the calculated robot relative speeds)
-     * @return current ChassisSpeeds
-     */
-    public ChassisSpeeds getRobotRelativeSpeeds(){
-        return currentSpeeds;
+    @Override
+    public void simulationPeriodic() {
+        Robot.FIELD.setRobotPose(getCurrentOdometryPosition());
     }
 
     /**
@@ -126,19 +132,6 @@ public class Swerve extends SubsystemBase {
         return swerve.sysIdDynamic(direction);
     }
 
-    @Override
-    public void periodic() {
-        Logger.recordOutput(SWERVE.LOG_PATH+"Current Pose", getCurrentOdometryPosition());
-
-        //TODO: do we really need to run this? 
-        swerve.periodic();
-    }
-
-    @Override
-    public void simulationPeriodic() {
-        Robot.FIELD.setRobotPose(getCurrentOdometryPosition());
-    }
-
     /**
      * Send a {@code ChassisSpeeds} to the drivetrain, field-relative
      *
@@ -147,17 +140,18 @@ public class Swerve extends SubsystemBase {
     public void drive(ChassisSpeeds speeds) {
         Logger.recordOutput(SWERVE.LOG_PATH+"TargetSpeeds", speeds); 
 
-        swerve.setControl(
-            fieldCentric.withVelocityX(speeds.vxMetersPerSecond)
-            .withVelocityY(speeds.vyMetersPerSecond) 
-            .withRotationalRate(speeds.omegaRadiansPerSecond)
-        );
+        ChassisSpeeds fieldCentricSpeeds = new ChassisSpeeds();
+        fieldCentricSpeeds = fieldCentricSpeeds.fromRobotRelativeSpeeds(speeds, getYaw());
 
-        currentSpeeds = speeds;
+        swerve.setControl(
+            fieldCentric.withVelocityX(fieldCentricSpeeds.vxMetersPerSecond)
+            .withVelocityY(fieldCentricSpeeds.vyMetersPerSecond) 
+            .withRotationalRate(fieldCentricSpeeds.omegaRadiansPerSecond)
+        );
     }
 
     /**
-     * Sends a robot-relative ChassisSpeeds to the drivetrain
+     * Sends a robot-relative ChassisSpeeds to the drivetrain (required for PathPlanner)
      * @param speeds robot-relative ChassisSpeeds to run the drivetrain at
      */
     public void driveRobotRelative(ChassisSpeeds speeds){
@@ -166,12 +160,43 @@ public class Swerve extends SubsystemBase {
             .withVelocityY(speeds.vyMetersPerSecond)
             .withRotationalRate(speeds.omegaRadiansPerSecond)
         );
+    }
 
-        currentSpeeds = speeds;
+    //TODO: figure out how to implement the feedforwards object to drive the swerve
+    //required for the pathplanner to have the feedforwards object
+    public void driveRobotRelative(ChassisSpeeds speeds, DriveFeedforwards feedforwards){
+        swerve.setControl(
+            robotCentric.withVelocityX(speeds.vxMetersPerSecond)
+            .withVelocityY(speeds.vyMetersPerSecond)
+            .withRotationalRate(speeds.omegaRadiansPerSecond)
+        );
     }
 
     /**
-     * Define whatever direction the robot is facing as forward
+     * Gets the current robot-relative speed of the robot
+     * 
+     * @return a ChassisSpeeds speed
+     */
+    public ChassisSpeeds getRobotRelativeSpeeds(){
+        return swerve.getState().Speeds;
+    }
+
+    //TODO: method to drive swerve in a circle around the hub from the current position
+    public ChassisSpeeds driveInCircle(){
+        double hubLocationX = 4.02844;
+        double hubLocationY = 4.445;
+
+        double robotX = getCurrentOdometryPosition().getX();
+        double robotY = getCurrentOdometryPosition().getY();
+
+        double dx = robotX - hubLocationX;
+        double dy = robotY - hubLocationY;
+
+        double radius = Math.hypot(dx, dy);
+    }
+
+    /**
+     * Define the direction the robot is facing as forward
      */
     public void resetHeading(){
         swerve.seedFieldCentric();
@@ -181,15 +206,7 @@ public class Swerve extends SubsystemBase {
      * Stop the swerve (feed zeros for all target velocities)
      */
     public void stop(){
-        drive(speedZero);
-    }
-
-    /**
-     * Commands the swerve to stop
-     * @return a command to stop the drivetrain
-     */
-    public Command stopCommand(){
-        return swerve.runOnce(() -> stop());
+        drive(new ChassisSpeeds());
     }
 
     /**
@@ -201,6 +218,7 @@ public class Swerve extends SubsystemBase {
 
     /**
      * Get the rotation of the current robot pose
+     * 
      * @return Rotation2d robot rotation
      */
     public Rotation2d getYaw() {
@@ -209,30 +227,25 @@ public class Swerve extends SubsystemBase {
 
     /**
      * Get the current pose of the robot
+     * 
      * @return Pose2d robot pose
      */
     public Pose2d getCurrentOdometryPosition() {
         return swerve.getState().Pose;
     }
 
-    public void setKnownOdometryPose(Pose2d currentPose) { 
+    /**
+     * Sets the odometry pose of the robot to the given pose
+     * 
+     * @param currentPose new robot pose
+     */
+    public void resetPose(Pose2d currentPose) { 
         swerve.resetPose(currentPose);
 
-        Logger.recordOutput(
-            SWERVE.LOG_PATH+"Console", (
-                "X: " +
-                currentPose.getX()+
-                "; Y: " +
-                currentPose.getY() +
-                "; Rotation: " +
-                currentPose.getRotation().getDegrees() +
-                "°."
-            )
-        );
     }
 
     /**
-     * Sets whether or not the robot runs at a slower speed
+     * Sets if the robot runs at a slower speed
      * 
      * @param slowMode whether to slow the drivetrain
      */
@@ -241,25 +254,15 @@ public class Swerve extends SubsystemBase {
     }
     
     /**
-     * Use PID to snap the robot to a rotational setpoint
+     * Use PID to snap the robot to a targeted rotational heading
      *
-     * @param setpoint the setpoint to snap to
+     * @param targetHeading the heading to snap to
      * @return the rotational velocity setpoint as a Rotation2d
      */
-    public double snapToAngle(Rotation2d setpoint) {
-        double currYaw = Math.toRadians(getYaw().getDegrees()%360);
-        double errorAngle = setpoint.getRadians() - currYaw;
+    public double snapToAngle(Rotation2d targetHeading) {
+        double currYaw = getYaw().getRadians();
 
-        if(errorAngle > Math.PI){
-            errorAngle -= 2 * Math.PI;
-        }
-        else if(errorAngle <= -Math.PI){
-            errorAngle += 2 * Math.PI;
-        }
-
-        double out = snapToController.calculate(0, errorAngle);
-
-        return out;
+        return snapToController.calculate(currYaw, targetHeading.getRadians());
     }
 
     /**
@@ -311,10 +314,88 @@ public class Swerve extends SubsystemBase {
 
     /**
      * Corrects the robot odometry using vision
+     * 
      * @param visionRobotPoseMeters The robot pose using vision measuremnets
      * @param timestampSeconds Timestamp of the vision measurement in seconds
      */
     public void addVisionMeasurement(Pose2d visionRobotPoseMeters, double timestampSeconds) {
         swerve.addVisionMeasurement(visionRobotPoseMeters, timestampSeconds);
     }
+
+    /**
+     * A method by PathPlanner that allows you to run a path outside of autos
+     * 
+     * @param pathName name of path to run
+     * @param choreoPath whether the path is from Choreo or not
+     * @return a command to run the swerve along the path
+     */
+    public Command followPathCommand(String pathName, boolean choreoPath) {
+        try{
+            PathPlannerPath path;
+            if(!choreoPath)
+                path = PathPlannerPath.fromPathFile(pathName);
+            else 
+                path = PathPlannerPath.fromChoreoTrajectory(pathName);
+
+            return new FollowPathCommand(
+                    path,
+                    this::getCurrentOdometryPosition, // Robot pose supplier
+                    this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                    this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds, AND feedforwards
+                    //TODO: tune the pid constants
+                    new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+                            new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                            new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+                    ),
+
+                    config, // The robot configuration
+
+                    () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+                        var alliance = DriverStation.getAlliance();
+                        if (alliance.isPresent()) {
+                            return alliance.get() == DriverStation.Alliance.Red;
+                        }
+
+                        return false;
+                    },
+                    
+                    this // Reference to this subsystem to set requirements
+            );
+
+        } catch (Exception e) {
+            DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
+            return Commands.none();
+        }
+    }
+
+    //TODO: don't let the robot drive into obstacles...
+    public Command generatePath(List<Pose2d> poses){
+        List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(poses);
+        //TODO: figure out how to set these values correctly for path constraints
+        PathConstraints constaints = new PathConstraints(null, null, null, null);
+
+        PathPlannerPath path = new PathPlannerPath(
+            waypoints, 
+            constaints, 
+            null, //null in order to start from your location on the field
+            new GoalEndState(0, poses.get(poses.size() - 1).getRotation())
+        );
+
+        return AutoBuilder.followPath(path);
+    }
+
+    //TODO: a method for on the fly pathing (i.e. from current position to xx position)
+    //TODO: don't let it drive into obstacles!
+    public Command pathfindToTarget(Pose2d targetPose){
+
+        return Commands.none();
+    }
+
+    
+
 }
+
+    
