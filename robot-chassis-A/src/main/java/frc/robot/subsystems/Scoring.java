2a4e1db633922dbe0ac453e5f37488472af3256f
yuii01 <whitecoralflame@gmail.com>
2026-02-22 11:39:28 -0500

    reimplemented aiming to red hub in scoring.java

--- robot-chassis-A/src/main/java/frc/robot/subsystems/Scoring.java
+++ robot-chassis-A/src/main/java/frc/robot/subsystems/Scoring.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.subsystems.swerve.Swerve;
@@ -14,10 +15,16 @@ public class Scoring extends SubsystemBase{
     public Scoring(Swerve swerve){
         this.swerve = swerve;
 
-        turret = new Turret(swerve);
+        turret = new Turret();
         shooter = new Shooter();
         intake = new Intake();
         indexer = new Indexer();
+    }
+
+    @Override
+    public void periodic(){
+        //will currently only track the red hub
+        turret.TurrettoAngle(swerve.getCurrentOdometryPosition(), new Pose2d(4.02844, 4.445, swerve.getYaw()));
     }
     
 }
