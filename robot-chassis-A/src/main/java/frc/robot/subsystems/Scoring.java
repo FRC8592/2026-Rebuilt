71049b945015c0914e0f2b944e0f5e04c35b16cf
yuii01 <whitecoralflame@gmail.com>
2026-02-22 11:04:04 -0500

    moved classes to under scoring and cleaned up odometry/vision/swerve

--- robot-chassis-A/src/main/java/frc/robot/subsystems/Scoring.java
+++ robot-chassis-A/src/main/java/frc/robot/subsystems/Scoring.java
@@ -1,29 +1,23 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.subsystems.swerve.Swerve;
-
-
 
 public class Scoring extends SubsystemBase{
     Swerve swerve;
     Turret turret;
     Shooter shooter;
-
+    Indexer indexer;
+    Intake intake;
 
     public Scoring(Swerve swerve){
         this.swerve = swerve;
 
         turret = new Turret(swerve);
         shooter = new Shooter();
-    }
-
-    public Command autoTurretCommand(){
-        return turret.TurrettoPosCommand(new Pose2d(4.02844, 4.445, swerve.getYaw()));
+        intake = new Intake();
+        indexer = new Indexer();
     }
     
 }
