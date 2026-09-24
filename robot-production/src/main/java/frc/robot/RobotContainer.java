2951a69f363793c7c522acf668b7f0dd3312df86
baaisha <naaisha.mahajan@gmail.com>
2026-09-24 00:35:13 -0400

    Added new control implementations according to issue.

--- robot-production/src/main/java/frc/robot/RobotContainer.java
+++ robot-production/src/main/java/frc/robot/RobotContainer.java
@@ -49,23 +49,32 @@ public class RobotContainer {
   // Driver Controls
   private final Trigger RESET_HEADING = driverController.back();
   private final Trigger ROBOT_RELATIVE = driverController.y();
-  private final Trigger SLOW_MODE = driverController.a();
+  private final Trigger SLOW_MODE = driverController.b();
   private final Trigger LESS_SLOW_MODE = driverController.povUp();
-  private final Trigger INTAKE_RUN = driverController.rightTrigger();
-  private final Trigger INTAKE_REVERSE = driverController.rightBumper();
+
+  private final Trigger INTAKE_RUN = driverController.leftTrigger();
   private final Trigger INTAKE_EXTEND = driverController.leftBumper();
-  private final Trigger INTAKE_RETRACT = driverController.leftTrigger();
-  private final Trigger RESET_EXTEND = driverController.b();
+  private final Trigger ENABLE_TRACKING = driverController.rightTrigger();
+  private final Trigger SHOOT = driverController.rightBumper();
+
+  private final Trigger INTAKE_REVERSE = driverController.a();
+
+  // private final Trigger RESET_EXTEND = driverController.b();
   private final Trigger LOCK_WHEELS = driverController.x();
   // private final Trigger SHOOT_SQUEEZE = driverController.a();
 
+
+
+
   // Operator Controls
-  private final Trigger ENABLE_TRACKING = operatorController.leftTrigger();
-  private final Trigger SHOOT = operatorController.rightTrigger();
+
   private final Trigger SHOOT_REVERSE = operatorController.leftBumper(); //originally right bumper 
+  private final Trigger DYEROTOR_UNJAM = operatorController.leftTrigger();
+  private final Trigger INTAKE_RETRACT = operatorController.rightTrigger();
 
   // private final Trigger RESET_TURRET = operatorController.a();// originally on button a 
-  private final Trigger MANUAL_OVERRIDE = operatorController.rightBumper();
+  private final Trigger MANUAL_OVERRIDE = operatorController.rightBumper(); // Also known as Turret Lock 
+
   private final Trigger INCREASE_RPM = operatorController.povUp();
   private final Trigger DECREASE_RPM = operatorController.povDown();
   private final Trigger TURRET_RIGHT = operatorController.povRight();
@@ -165,6 +174,7 @@ public class RobotContainer {
 
     SHOOT.onTrue(scoring.indexer.runIndexerCommand()).onFalse(scoring.indexer.stopCommand());
     SHOOT_REVERSE.onTrue(scoring.indexer.runReverseIndexerCommand()).onFalse (scoring.indexer.stopCommand()); 
+    DYEROTOR_UNJAM.onTrue(scoring.indexer.runReverseIndexerCommand()).onFalse(scoring.indexer.stopCommand());
 
     // RESET_TURRET.onTrue(scoring.turret.resetPosCommand());
 
